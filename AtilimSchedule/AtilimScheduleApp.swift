69c8594971393d2fed0841475513eb9cfedc175e
Ali Al-Fatlawi <alawi_amer@yahoo.co.uk>
2023-10-05 23:04:11 +0300

    Initial Commit

---
+++ AtilimSchedule/AtilimScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtilimScheduleApp.swift
+//  AtilimSchedule
+//
+//  Created by Ali Amer on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AtilimScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
