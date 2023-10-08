e73e609b88845565670470cad4518fd1ed0f2de3
ErkinArikan <info@biligoba.com>
2023-10-08 23:07:46 +0300

    new schedule view

---
+++ AtilimSchedule/View/SelectCourseView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectCourseView.swift
+//  AtilimSchedule
+//
+//  Created by Tacettin Pekin on 8.10.2023.
+//
+
+import SwiftUI
+
+struct SelectCourseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectCourseView()
+}
