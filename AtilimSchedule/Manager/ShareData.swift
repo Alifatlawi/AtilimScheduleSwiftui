7be49e3018a7900df473fc647ad5d50b395b4ed0
Ali Al-Fatlawi <alawi_amer@yahoo.co.uk>
2023-10-16 23:25:05 +0300

    added other faculties

---
+++ AtilimSchedule/Manager/ShareData.swift
@@ -0,0 +1,8 @@
+//
+//  ShareData.swift
+//  AtilimSchedule
+//
+//  Created by Ali Amer on 10/16/23.
+//
+
+import Foundation
---
+++ AtilimSchedule/Model/ AvmModel.swift
@@ -0,0 +1,8 @@
+//
+//   AvmModel.swift
+//  AtilimSchedule
+//
+//  Created by Ali Amer on 10/16/23.
+//
+
+import Foundation
